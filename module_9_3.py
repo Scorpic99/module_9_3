2a76ca52250ac247882c6e7860833219a358e4bc
Александр <116272931+Scorpic99@users.noreply.github.com>
2024-11-11 23:47:13 +0300

    Update module_9_3_upd.py

--- module_9_3.py
+++ module_9_3.py
@@ -1,8 +1,7 @@
 first = ['Strings', 'Student', 'Computers']
 second = ['Строка', 'Урбан', 'Компьютер']
 
-zp = zip(first, second)
-first_result = (len(x[0]) - len(x[1]) for x in zp if len(x[0]) != len(x[1]))
+first_result = (len(x[0]) - len(x[1]) for x in zip(first, second) if len(x[0]) != len(x[1]))
 second_result = (len(first[x]) == len(second[x]) for x in range(len(first)))
 
 
